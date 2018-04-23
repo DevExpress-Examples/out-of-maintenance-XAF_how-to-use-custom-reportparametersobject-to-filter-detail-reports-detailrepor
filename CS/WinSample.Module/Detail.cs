70434e5cda447807315a502dabe51d45e97bc16a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WinSample.Module/Detail.cs
@@ -0,0 +1,31 @@
+using System;
+
+using DevExpress.Xpo;
+
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace WinSample.Module {
+    [DefaultClassOptions]
+    public class Detail : BaseObject {
+        public Detail(Session session) : base(session) { }
+        private string _DetailName;
+        public string DetailName {
+            get { return _DetailName; }
+            set { SetPropertyValue("DetailName", ref _DetailName, value); }
+        }
+        private Master _Master;
+        [Association("Master-Details")]
+        public Master Master {
+            get { return _Master; }
+            set { SetPropertyValue("Master", ref _Master, value); }
+        }
+        private DateTime _StartOn;
+        public DateTime StartOn {
+            get { return _StartOn; }
+            set { SetPropertyValue("StartOn", ref _StartOn, value); }
+        }
+    }
+}
---
+++ VB/WinSample.Module/Updater.vb
@@ -0,0 +1,34 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.ExpressApp.Reports
+Imports DevExpress.ExpressApp
+
+Namespace WinSample.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As DevExpress.ExpressApp.IObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+			CreateReport("MasterDetailsReport")
+		End Sub
+		Private Sub CreateReport(ByVal reportName As String)
+			Dim reportdata As ReportData = Session.FindObject(Of ReportData)(New BinaryOperator("Name", reportName))
+			If reportdata Is Nothing Then
+				reportdata = New ReportData(Session)
+				Dim rep As New XafReport()
+				rep.ReportName = reportName
+				rep.ObjectSpace = New ObjectSpace(New UnitOfWork(Session.DataLayer), XafTypesInfo.Instance)
+				rep.LoadLayout(Me.GetType().Assembly.GetManifestResourceStream("WinSample.Module.EmbeddedReports." & reportName & ".repx"))
+				reportdata.SaveXtraReport(rep)
+				reportdata.Save()
+			End If
+		End Sub
+	End Class
+End Namespace
